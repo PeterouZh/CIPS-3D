f4d7aafa7068ed95481a1c5a837bc9c28d1c1664
Zhou Peng <zhoupengcv@sjtu.edu.cn>
2022-01-09 19:43:32 +0800

    v1

--- exp/tests/test_cips3d.py
+++ exp/tests/test_cips3d.py
@@ -335,7 +335,7 @@ class Testing_ffhq_exp(unittest.TestCase):
     dd[f'{bucket_root}/results/CIPS-3D/ffhq_exp/train_ffhq-20220102_121420_779'] = \
       {'20220102_121420_779-ffhq_r128-gpu.8x4-aux.F': f"{log_file}", }
     dd[f'{bucket_root}/results/CIPS-3D/ffhq_exp/train_ffhq-20220106_090056_403'] = \
-      {'20220106_090056_403-ffhq_r128-gpu.8x4-aux.F': f"{log_file}", }
+      {'20220106_090056_403-ffhq_r128-gpu.8x4-aux.F-warmupD.T': f"{log_file}", }
 
     dd['properties'] = {'title': title,
                         # 'xlim': [0, 3000000],
@@ -1066,6 +1066,66 @@ class Testing_ffhq_exp(unittest.TestCase):
 
 class Testing_ffhq_exp_v1(unittest.TestCase):
 
+  def test__plot_fid(self):
+    """
+    export PYTHONPATH=.:./tl2_lib
+    python -c "from exp.tests.test_cips3d import Testing_ffhq_exp;\
+      Testing_ffhq_exp().test__plot_fid()"
+
+    """
+    if 'CUDA_VISIBLE_DEVICES' not in os.environ:
+      os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    if 'TIME_STR' not in os.environ:
+      os.environ['TIME_STR'] = '0'
+    from tl2.launch.launch_utils import \
+      (get_command_and_outdir, setup_outdir_and_yaml, get_append_cmd_str, start_cmd_run)
+
+    command, outdir = get_command_and_outdir(self, func_name=sys._getframe().f_code.co_name, file=__file__)
+    argv_str = f"""
+                  --tl_config_file none
+                  --tl_command none
+                  --tl_outdir {outdir}
+                  """
+    args = setup_outdir_and_yaml(argv_str)
+    outdir = args.tl_outdir
+
+    from tl2.proj.matplot.plot_results import PlotResults
+    import collections
+    import pickle
+
+    outfigure = os.path.join(outdir, 'FID.jpg')
+    default_dicts = collections.OrderedDict()
+    show_max = []
+
+    bucket_root = "/home/ma-user/work/ZhouPeng/bucket_3690/"
+
+    FID_r64 = collections.defaultdict(dict)
+    title = 'FID_r64'
+    log_file = 'textdir/eval.ma0.FID.log'
+    dd = eval(title)
+    dd[f'{bucket_root}/results/CIPS-3D/ffhq_exp_v1/train_ffhq-20220109_174243_622'] = \
+      {'20220109_174243_622-ffhq_r64-gpu.8x4': f"{log_file}", }
+
+    dd['properties'] = {'title': title,
+                        # 'xlim': [0, 3000000],
+                        # 'ylim': [0, 100]
+                        }
+    default_dicts[title] = dd
+    show_max.append(False)
+
+
+    plotobs = PlotResults()
+    label2datas_list = plotobs.plot_defaultdicts(
+      outfigure=outfigure, default_dicts=default_dicts, show_max=show_max, figsize_wh=(16, 7.2))
+    print(f'Save to {outfigure}.')
+
+    saved_data = '__'.join(outdir.split('/')[-2:])
+    saved_data = f"{outdir}/{saved_data}.pkl"
+    with open(saved_data, 'wb') as f:
+      pickle.dump(label2datas_list, f)
+    print(f"Save data to {saved_data}")
+    pass
+
   def test__build_generator(self, debug=True):
     """
     Usage:
@@ -1111,6 +1171,7 @@ class Testing_ffhq_exp_v1(unittest.TestCase):
                 --tl_outdir {outdir}
                 {"--tl_resume --tl_resumedir " + outdir if resume else ""}
                 --tl_opts {tl_opts}
+                --tl_debug
                 """
     args, cfg = setup_outdir_and_yaml(argv_str, return_cfg=True)
 
@@ -1138,6 +1199,108 @@ class Testing_ffhq_exp_v1(unittest.TestCase):
     img_pil = tv_f.to_pil_image(imgs)
     pil_utils.imshow_pil(img_pil)
 
+    pass
+
+  def test_train_ffhq(self, debug=True):
+    """
+    Usage:
+
+        # export CUDA_VISIBLE_DEVICES=$cuda_devices
+        # export RUN_NUM=$run_num
+
+        export CUDA_VISIBLE_DEVICES=0,1
+        export PORT=12345
+        export TIME_STR=0
+        export PYTHONPATH=.:./tl2_lib
+        python -c "from exp.tests.test_cips3d import Testing_ffhq_exp_v1;\
+          Testing_ffhq_exp_v1().test_train_ffhq(debug=False)" \
+          --tl_opts \
+            batch_size 4 img_size 64  warmup_D True \
+          --tl_outdir results/ffhq_exp/train_ffhq
+
+
+    :return:
+    """
+    if 'CUDA_VISIBLE_DEVICES' not in os.environ:
+      os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+    if 'TIME_STR' not in os.environ:
+      os.environ['TIME_STR'] = '0'
+    if 'RUN_NUM' not in os.environ:
+      os.environ['RUN_NUM'] = '0'
+    from tl2 import tl2_utils
+    from tl2.launch.launch_utils import \
+      (get_command_and_outdir, setup_outdir_and_yaml, get_append_cmd_str, start_cmd_run)
+
+    tl_opts_list = tl2_utils.parser_args_from_list(name="--tl_opts", argv_list=sys.argv, type='list')
+    tl_opts = ' '.join(tl_opts_list)
+    print(f'tl_opts:\n {tl_opts}')
+
+    if debug:
+      # sys.argv.extend(['--tl_outdir', 'results/ffhq_exp/train_ffhq'])
+      pass
+    command, outdir = get_command_and_outdir(self, func_name=sys._getframe().f_code.co_name, file=__file__)
+    resume = os.path.isdir(f"{outdir}/ckptdir/resume") and \
+             tl2_utils.parser_args_from_list(name="--tl_outdir", argv_list=sys.argv, type='str') is not None
+    argv_str = f"""
+                --tl_config_file exp/cips3d/configs/ffhq_exp_v1.yaml
+                --tl_command {command}
+                --tl_outdir {outdir}
+                {"--tl_resume --tl_resumedir " + outdir if resume else ""}
+                --tl_opts {tl_opts}
+                """
+    args, cfg = setup_outdir_and_yaml(argv_str, return_cfg=True)
+
+    if int(os.environ['RUN_NUM']) > 0:
+      run_command = f"""
+                  python -c "from tl2.modelarts.tests.test_run import TestingRun;\
+                        TestingRun().test_run_v2(number={os.environ['RUN_NUM']}, )" \
+                        --tl_opts root_obs {cfg.root_obs}
+                  """
+      p = tl2_utils.Worker(name='Run', args=(run_command,))
+      p.start()
+
+    n_gpus = len(os.environ['CUDA_VISIBLE_DEVICES'].split(','))
+    PORT = os.environ.get('PORT', 12345)
+
+    os.environ['DNNLIB_CACHE_DIR'] = "cache_dnnlib"
+    os.environ['TORCH_EXTENSIONS_DIR'] = "cache_torch_extensions"
+    os.environ['PATH'] = f"{os.path.dirname(sys.executable)}:{os.environ['PATH']}"
+    os.environ['MAX_JOBS '] = "8"
+
+    cmd_str = f"""
+        python 
+        exp/cips3d/scripts/train.py
+        --port {PORT}
+
+        {get_append_cmd_str(args)}
+        """
+    if debug:
+      cmd_str += f"""
+                  --tl_debug
+                  --tl_opts num_workers 0
+                  """
+    else:
+      cmd_str += f"""
+                  --tl_opts num_workers {n_gpus} 
+                    {tl_opts}
+                  """
+    start_cmd_run(cmd_str)
+    # from tl2.launch.launch_utils import update_parser_defaults_from_yaml, global_cfg
+    # from tl2.modelarts import moxing_utils
+
+    # update_parser_defaults_from_yaml(parser)
+    # if rank == 0:
+    #   moxing_utils.setup_tl_outdir_obs(global_cfg)
+    #   moxing_utils.modelarts_sync_results_dir(global_cfg, join=True)
+
+    # moxing_utils.modelarts_sync_results_dir(global_cfg, join=True)
+
+    # modelarts_utils.setup_tl_outdir_obs(global_cfg)
+    # modelarts_utils.modelarts_sync_results_dir(global_cfg, join=True)
+    # modelarts_utils.prepare_dataset(global_cfg.get('modelarts_download', {}), global_cfg=global_cfg)
+    #
+    # modelarts_utils.prepare_dataset(global_cfg.get('modelarts_upload', {}), global_cfg=global_cfg, download=False)
+    # modelarts_utils.modelarts_sync_results_dir(global_cfg, join=True)
     pass
 
 
