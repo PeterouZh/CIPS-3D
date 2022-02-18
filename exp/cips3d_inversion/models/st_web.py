41400008288b489edfda6f0d64f276bbb2ff473b
Zhou Peng <zhoupengcv@sjtu.edu.cn>
2022-02-18 21:55:30 +0800

    interpolate_z_web

---
+++ exp/cips3d_inversion/models/st_web.py
@@ -0,0 +1,147 @@
+import math
+import pathlib
+import collections
+from pathlib import Path
+import logging
+import os
+import sys
+
+import numpy as np
+from PIL import Image
+import streamlit as st
+
+sys.path.insert(0, os.getcwd())
+
+import torch
+from torchvision.utils import make_grid
+import torchvision.transforms.functional as tv_f
+
+from tl2.launch.launch_utils import update_parser_defaults_from_yaml, global_cfg
+from tl2.proj.streamlit import SessionState
+from tl2.proj.streamlit import st_utils
+from tl2.proj.logger.logger_utils import get_file_logger
+from tl2 import tl2_utils
+from tl2.proj.streamlit import st_utils
+from tl2.proj.fvcore import build_model, MODEL_REGISTRY
+from tl2.proj.pytorch import torch_utils
+from tl2.proj.pytorch.examples.nerf import cam_params
+from tl2.proj.fvcore.checkpoint import Checkpointer
+from tl2.proj.pil import pil_utils
+from tl2.proj.cv2 import cv2_utils
+
+# sys.path.insert(0, f"{os.getcwd()}/DGP_lib")
+# from DGP_lib import utils
+# sys.path.pop(0)
+
+
+def interpolate_sphere(z1,
+                       z2,
+                       t):
+  p = (z1 * z2).sum(dim=-1, keepdim=True)
+  p = p / z1.pow(2).sum(dim=-1, keepdim=True).sqrt()
+  p = p / z2.pow(2).sum(dim=-1, keepdim=True).sqrt()
+  omega = torch.acos(p)
+  s1 = torch.sin((1 - t) * omega) / torch.sin(omega)
+  s2 = torch.sin(t * omega) / torch.sin(omega)
+  z = s1 * z1 + s2 * z2
+  return z
+
+
+@MODEL_REGISTRY.register(name_prefix=__name__)
+class STModel(object):
+  def __init__(self):
+
+    pass
+
+  def interpolate_z_web(self,
+                        cfg,
+                        outdir,
+                        saved_suffix_state=None,
+                        **kwargs):
+
+    network_pkl = st_utils.selectbox_v1('network_pkl', options_dict=cfg.network_pkl,
+                                        default_key=cfg.default_network_pkl, sidebar=True)
+    bs = st_utils.number_input('bs', cfg.bs, sidebar=True)
+    img_size = st_utils.number_input('img_size', cfg.img_size, sidebar=True)
+    N_samples = st_utils.number_input('N_samples', cfg.N_samples, sidebar=True)
+    N_step = st_utils.number_input('N_step', cfg.N_step, sidebar=True)
+    fps = st_utils.number_input('fps', cfg.fps, sidebar=True)
+
+    z_mode = st_utils.selectbox('z_mode', options=cfg.z_mode, default_value=cfg.default_z_mode, sidebar=True)
+    interp_mode = st_utils.selectbox('interp_mode', options=cfg.interp_mode, sidebar=True)
+
+    nerf_N_samples = st_utils.number_input('nerf_N_samples', cfg.nerf_N_samples, sidebar=True)
+    nerf_N_importance = st_utils.number_input('nerf_N_importance', cfg.nerf_N_importance, sidebar=True)
+
+    seed = st_utils.get_seed(seeds=cfg.seeds)
+
+    if not global_cfg.tl_debug:
+      if not st.sidebar.button("run_web"):
+        return
+
+    if saved_suffix_state is not None:
+      saved_suffix_state.saved_suffix = saved_suffix_state.saved_suffix + 1
+
+    device = 'cuda'
+    torch_utils.init_seeds(seed=seed)
+
+    G_kwargs = cfg.G_kwargs.clone()
+    G_kwargs['nerf_kwargs']['h_stddev'] = 0.
+    G_kwargs['nerf_kwargs']['v_stddev'] = 0.
+    G_kwargs['nerf_kwargs']['N_samples'] = nerf_N_samples
+    G_kwargs['nerf_kwargs']['N_importance'] = nerf_N_importance
+
+    G = build_model(cfg.G_cfg).cuda()
+    Checkpointer(G).load_state_dict_from_file(network_pkl)
+    # G = torch.load(network_pkl).cuda()
+
+    H = W = img_size
+    cam_param = cam_params.CamParams.from_config(H0=H, W0=W).cuda()
+
+    intr = cam_param(mode='get_intrinsic')
+    rays_o, rays_d, select_inds = cam_param.get_rays_random_pose(
+      device=device, bs=bs, intr=intr, **G_kwargs.nerf_kwargs)
+
+    st_image = st.empty()
+    video_f = cv2_utils.ImageioVideoWriter(f"{outdir}/video.mp4", fps=fps, hd_video=True)
+
+    G.eval()
+    zs = G.get_zs(bs)
+    zs_list = [G.get_zs(bs) for _ in range(N_samples)]
+
+    for idx in range(N_samples):
+      zs1 = zs_list[idx]
+      zs2 = zs_list[(idx + 1) % N_samples]
+      ts = np.linspace(0, 1, N_step)
+      for t in ts:
+        z1_ = zs1[z_mode]
+        z2_ = zs2[z_mode]
+        if interp_mode == 'slerp':
+          z_interp_ = interpolate_sphere(z1_, z2_, t)
+        elif interp_mode == 'lerp':
+          z_interp_ = torch.lerp(z1_, z2_, t)
+        else:
+          assert 0
+        zs[z_mode] = z_interp_
+
+        with torch.set_grad_enabled(False):
+          imgs, ret_imgs = G(zs=zs,
+                             rays_o=rays_o,
+                             rays_d=rays_d,
+                             forward_points=256 ** 2,  # disable gradients
+                             return_aux_img=True,
+                             **G_kwargs)
+        img = make_grid(imgs, nrow=int(math.sqrt(bs)), normalize=True, scale_each=True)
+        img_pil = tv_f.to_pil_image(img)
+        img_str = f"{idx}-{(idx+1)%N_samples}/{N_samples}, t={t:.2f}"
+        pil_utils.add_text(img_pil, img_str, size=img_pil.size[0]//18)
+
+        st_utils.st_image(img_pil, caption=f"{imgs.shape}",
+                          debug=global_cfg.tl_debug, st_empty=st_image)
+        video_f.write(img_pil)
+
+    video_f.release(st_video=True)
+
+
+
+    pass
--- exp/tests/test_cips3d_inversion.py
+++ exp/tests/test_cips3d_inversion.py
@@ -1,3 +1,4 @@
+import importlib
 import os
 import sys
 import unittest
@@ -497,18 +498,24 @@ class Testing_ffhq_diffcam_exp_v1(unittest.TestCase):
     title = 'FID_r64'
     log_file = 'textdir/eval.ma0.FID.log'
     dd = eval(title)
+    dd[f'{bucket_root}/results/CIPS-3D/ffhq_diffcam_exp/train_ffhq-20220124_160131_073'] = \
+      {'20220124_160131_073-ffhq_r64-gpu.4x8-finetune.F-far.1_12-gridwraper.T': f"{log_file}", }
     dd[f'{bucket_root}/results/CIPS-3D/ffhq_diffcam_exp_v1/train_ffhq-20220218_012019_645'] = \
       {'20220218_012019_645-ffhq_r128-gpu.4x4-': f"{log_file}", }
     dd[f'{bucket_root}/results/CIPS-3D/ffhq_diffcam_exp_v1/train_ffhq-20220218_023549_830'] = \
       {'20220218_023549_830-ffhq_r128-gpu.4x8-': f"{log_file}", }
     dd[f'{bucket_root}/results/CIPS-3D/ffhq_diffcam_exp_v1/train_ffhq-20220218_093950_696'] = \
       {'20220218_093950_696-ffhq_r128-gpu.4x8-gridscale.F': f"{log_file}", }
-    dd[f'{bucket_root}/results/CIPS-3D/ffhq_diffcam_exp_v1/train_ffhq-20220218_104824_186'] = \
-      {'20220218_104824_186-ffhq_r128-gpu.4x8-gridscale.F-appendxyz.F': f"{log_file}", }
+    # dd[f'{bucket_root}/results/CIPS-3D/ffhq_diffcam_exp_v1/train_ffhq-20220218_104824_186'] = \
+    #   {'20220218_104824_186-ffhq_r128-gpu.4x8-gridscale.F-appendxyz.F': f"{log_file}", }
+    dd[f'{bucket_root}/results/CIPS-3D/ffhq_diffcam_exp_v1/train_ffhq-20220218_113228_416'] = \
+      {'20220218_113228_416-ffhq_r128-gpu.4x8-gridscale.F-appendxyz.F': f"{log_file}", }
+    dd[f'{bucket_root}/results/CIPS-3D/ffhq_diffcam_exp_v1/train_ffhq-20220218_141818_940'] = \
+      {'20220218_141818_940-ffhq_r128-gpu.4x8-gridscale.F-appendxyz.F-xyz_affine.F': f"{log_file}", }
 
     dd['properties'] = {'title': title,
                         # 'xlim': [0, 3000000],
-                        # 'ylim': [0, 50]
+                        'ylim': [0, 100]
                         }
     default_dicts[title] = dd
     show_max.append(False)
@@ -596,7 +603,7 @@ class Testing_ffhq_diffcam_exp_v1(unittest.TestCase):
     metadata['nerf_kwargs']['v_stddev'] = 0.
 
     num_imgs = 4
-    H = W = 64
+    H = W = 128
     # N_rays = 1024
     N_rays = -1
 
@@ -604,8 +611,8 @@ class Testing_ffhq_diffcam_exp_v1(unittest.TestCase):
                                                  H0=H,
                                                  W0=W).cuda()
 
-    # ckpt_dir = "../bucket_3690/results/CIPS-3D/ffhq_diffcam_exp_v1/train_ffhq-20220218_023549_830/ckptdir/resume"
-    ckpt_dir = "results/ffhq_diffcam_exp_v1/train_ffhq/ckptdir/resume"
+    ckpt_dir = "../bucket_3690/results/CIPS-3D/ffhq_diffcam_exp_v1/train_ffhq-20220218_113228_416/ckptdir/resume"
+    # ckpt_dir = "results/ffhq_diffcam_exp_v1/train_ffhq/ckptdir/resume"
     model_dict = {
       'G_ema': G,
       'cam_param': cam_param
@@ -619,11 +626,11 @@ class Testing_ffhq_diffcam_exp_v1(unittest.TestCase):
     G.eval()
     zs = G.get_zs(num_imgs)
 
-    with torch.set_grad_enabled(True):
+    with torch.set_grad_enabled(False):
       imgs, ret_imgs = G(zs=zs,
                          rays_o=rays_o,
                          rays_d=rays_d,
-                         forward_points=None,  # disable gradients
+                         forward_points=256**2,  # disable gradients
                          return_aux_img=True,
                          **metadata)
     img = make_grid(imgs, nrow=2, normalize=True, scale_each=True)
@@ -736,6 +743,65 @@ class Testing_ffhq_diffcam_exp_v1(unittest.TestCase):
     # modelarts_utils.modelarts_sync_results_dir(global_cfg, join=True)
     pass
 
+  def test_interpolate_z_web(self, debug=True):
+    """
+    Usage:
+        source activate PyTorch-1.8
 
+        export CUDA_VISIBLE_DEVICES=0
+        export TIME_STR=0
+        export PYTHONPATH=.:./tl2_lib
+        python -c "from exp.tests.test_cips3d_inversion import Testing_ffhq_diffcam_exp_v1;\
+          Testing_ffhq_diffcam_exp_v1().test_interpolate_z_web(debug=False)" \
+          --tl_opts port 8501
+
+    :return:
+    """
+    if 'CUDA_VISIBLE_DEVICES' not in os.environ:
+      os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    if 'TIME_STR' not in os.environ:
+      os.environ['TIME_STR'] = '0'
+    from tl2 import tl2_utils
+    from tl2.launch.launch_utils import \
+      (get_command_and_outdir, setup_outdir_and_yaml, get_append_cmd_str, start_cmd_run)
+
+    tl_opts_list = tl2_utils.parser_args_from_list(name="--tl_opts", argv_list=sys.argv, type='list')
+    tl_opts = ' '.join(tl_opts_list)
+    print(f'tl_opts:\n {tl_opts}')
+
+    command, outdir = get_command_and_outdir(self, func_name=sys._getframe().f_code.co_name, file=__file__)
+    argv_str = f"""
+                --tl_config_file exp/cips3d_inversion/configs/ffhq_diffcam_exp_v1.yaml
+                --tl_command {command}
+                --tl_outdir {outdir}
+                --tl_opts {tl_opts}
+                """
+    args, cfg = setup_outdir_and_yaml(argv_str, return_cfg=True)
+
+    n_gpus = len(os.environ['CUDA_VISIBLE_DEVICES'].split(','))
+
+    # script = "tl2_lib/tl2/proj/streamlit/scripts/run_web.py"
+    script = importlib.import_module('tl2.proj.streamlit.scripts.run_web').__file__
+    if debug:
+      cmd_str = f"""
+          python 
+            {script}
+            {get_append_cmd_str(args)}
+            --tl_debug
+            --tl_opts
+              """
+    else:
+      cmd_str_prefix = f"""
+              {os.path.dirname(sys.executable)}/streamlit run --server.port {cfg.port} 
+              {script}
+              --
+            """
+      cmd_str = f"""
+          {cmd_str_prefix}
+            {get_append_cmd_str(args)}
+            --tl_opts {tl_opts}
+        """
+    start_cmd_run(cmd_str)
+    pass
 
 
