fb8df119145c0d7048aa1cee437b481e0e485d19
Zhou Peng <zhoupengcv@sjtu.edu.cn>
2022-02-19 12:57:58 +0800

    sampling_yaw_web

--- exp/cips3d_inversion/models/st_web.py
+++ exp/cips3d_inversion/models/st_web.py
@@ -61,17 +61,22 @@ class STModel(object):
 
     network_pkl = st_utils.selectbox_v1('network_pkl', options_dict=cfg.network_pkl,
                                         default_key=cfg.default_network_pkl, sidebar=True)
+
+    st_utils.st_set_sep('video kwargs')
     bs = st_utils.number_input('bs', cfg.bs, sidebar=True)
     img_size = st_utils.number_input('img_size', cfg.img_size, sidebar=True)
     N_samples = st_utils.number_input('N_samples', cfg.N_samples, sidebar=True)
     N_step = st_utils.number_input('N_step', cfg.N_step, sidebar=True)
     fps = st_utils.number_input('fps', cfg.fps, sidebar=True)
 
+    st_utils.st_set_sep('interp kwargs')
     z_mode = st_utils.selectbox('z_mode', options=cfg.z_mode, default_value=cfg.default_z_mode, sidebar=True)
     interp_mode = st_utils.selectbox('interp_mode', options=cfg.interp_mode, sidebar=True)
 
+    st_utils.st_set_sep('NeRF kwargs')
     nerf_N_samples = st_utils.number_input('nerf_N_samples', cfg.nerf_N_samples, sidebar=True)
     nerf_N_importance = st_utils.number_input('nerf_N_importance', cfg.nerf_N_importance, sidebar=True)
+    forward_points = st_utils.number_input('forward_points', cfg.forward_points, sidebar=True)
 
     seed = st_utils.get_seed(seeds=cfg.seeds)
 
@@ -93,7 +98,6 @@ class STModel(object):
 
     G = build_model(cfg.G_cfg).cuda()
     Checkpointer(G).load_state_dict_from_file(network_pkl)
-    # G = torch.load(network_pkl).cuda()
 
     H = W = img_size
     cam_param = cam_params.CamParams.from_config(H0=H, W0=W).cuda()
@@ -128,7 +132,7 @@ class STModel(object):
           imgs, ret_imgs = G(zs=zs,
                              rays_o=rays_o,
                              rays_d=rays_d,
-                             forward_points=256 ** 2,  # disable gradients
+                             forward_points=forward_points ** 2,  # disable gradients
                              return_aux_img=True,
                              **G_kwargs)
         img = make_grid(imgs, nrow=int(math.sqrt(bs)), normalize=True, scale_each=True)
@@ -142,6 +146,95 @@ class STModel(object):
 
     video_f.release(st_video=True)
 
+    pass
 
+  def sampling_yaw_web(self,
+                       cfg,
+                       outdir,
+                       saved_suffix_state=None,
+                       **kwargs):
+
+    network_pkl = st_utils.selectbox_v1('network_pkl', options_dict=cfg.network_pkl,
+                                        default_key=cfg.default_network_pkl, sidebar=True)
+    network_pkl_model = st_utils.selectbox_v1('network_pkl_model', options_dict=cfg.network_pkl_model,
+                                              default_key=cfg.default_network_pkl_model, sidebar=True)
+    use_network_pkl_model = st_utils.checkbox('use_network_pkl_model', cfg.use_network_pkl_model)
+
+    st_utils.st_set_sep('video kwargs')
+    bs = st_utils.number_input('bs', cfg.bs, sidebar=True)
+    img_size = st_utils.number_input('img_size', cfg.img_size, sidebar=True)
+    fps = st_utils.number_input('fps', cfg.fps, sidebar=True)
+
+    st_utils.st_set_sep('NeRF kwargs')
+    nerf_N_samples = st_utils.number_input('nerf_N_samples', cfg.nerf_N_samples, sidebar=True)
+    nerf_N_importance = st_utils.number_input('nerf_N_importance', cfg.nerf_N_importance, sidebar=True)
+    forward_points = st_utils.number_input('forward_points', cfg.forward_points, sidebar=True)
+
+    st_utils.st_set_sep('pose kwargs')
+    yaw_min = st_utils.number_input('yaw_min', cfg.yaw_min, sidebar=True, format="%.3f")
+    yaw_max = st_utils.number_input('yaw_max', cfg.yaw_max, sidebar=True, format="%.3f")
+    N_step = st_utils.number_input('N_step', cfg.N_step, sidebar=True)
+
+    seed = st_utils.get_seed(seeds=cfg.seeds)
+
+    if not global_cfg.tl_debug:
+      if not st.sidebar.button("run_web"):
+        return
+
+    if saved_suffix_state is not None:
+      saved_suffix_state.saved_suffix = saved_suffix_state.saved_suffix + 1
+
+    device = 'cuda'
+    torch_utils.init_seeds(seed=seed)
+
+    G_kwargs = cfg.G_kwargs.clone()
+    G_kwargs['nerf_kwargs']['h_stddev'] = 0.
+    G_kwargs['nerf_kwargs']['v_stddev'] = 0.
+    G_kwargs['nerf_kwargs']['N_samples'] = nerf_N_samples
+    G_kwargs['nerf_kwargs']['N_importance'] = nerf_N_importance
+
+    if use_network_pkl_model:
+      G = torch.load(network_pkl_model).cuda()
+    else:
+      G = build_model(cfg.G_cfg).cuda()
+      Checkpointer(G).load_state_dict_from_file(network_pkl)
+
+    H = W = img_size
+    cam_param = cam_params.CamParams.from_config(H0=H, W0=W).cuda()
+    intr = cam_param(mode='get_intrinsic')
+
+
+    st_image = st.empty()
+    video_f = cv2_utils.ImageioVideoWriter(f"{outdir}/video.mp4", fps=fps, hd_video=True)
+
+    G.eval()
+    zs = G.get_zs(bs)
+
+    yaw_list = np.linspace(yaw_min, yaw_max, N_step)
+    for idx, yaw_ in enumerate(yaw_list):
+      rays_o, rays_d, select_inds = cam_param.get_rays_random_pose(
+        device=device, bs=bs, intr=intr,
+        **{**G_kwargs.nerf_kwargs,
+           'h_mean': yaw_,
+           'h_stddev': 0,
+           'v_stddev': 0})
+
+      with torch.set_grad_enabled(False):
+        imgs, ret_imgs = G(zs=zs,
+                           rays_o=rays_o,
+                           rays_d=rays_d,
+                           forward_points=forward_points ** 2,  # disable gradients
+                           return_aux_img=True,
+                           **G_kwargs)
+      img = make_grid(imgs, nrow=int(math.sqrt(bs)), normalize=True, scale_each=True)
+      img_pil = tv_f.to_pil_image(img)
+      img_str = f"{idx}/{N_step}, yaw={yaw_:.2f}"
+      pil_utils.add_text(img_pil, img_str, size=img_pil.size[0] // 18)
+
+      st_utils.st_image(img_pil, caption=f"{imgs.shape}",
+                        debug=global_cfg.tl_debug, st_empty=st_image)
+      video_f.write(img_pil)
+
+    video_f.release(st_video=True)
 
     pass
--- exp/tests/test_cips3d_inversion.py
+++ exp/tests/test_cips3d_inversion.py
@@ -458,6 +458,7 @@ class Testing_ffhq_diffcam_exp(unittest.TestCase):
 class Testing_ffhq_diffcam_exp_v1(unittest.TestCase):
   """
   NeRF: cips_net;
+  geometry problem;
 
   """
 
@@ -518,6 +519,20 @@ class Testing_ffhq_diffcam_exp_v1(unittest.TestCase):
     dd['properties'] = {'title': title,
                         # 'xlim': [0, 3000000],
                         'ylim': [0, 50]
+                        }
+    default_dicts[title] = dd
+    show_max.append(False)
+
+    FID_r128 = collections.defaultdict(dict)
+    title = 'FID_r128'
+    log_file = 'textdir/eval.ma0.FID.log'
+    dd = eval(title)
+    dd[f'{bucket_root}/results/CIPS-3D/ffhq_diffcam_exp_v1/train_ffhq-20220219_104511_901'] = \
+      {'20220219_104511_901-ffhq_r128-gpu.4x8-gridscale.F-appendxyz.F-xyz_affine.T': f"{log_file}", }
+
+    dd['properties'] = {'title': title,
+                        # 'xlim': [0, 3000000],
+                        # 'ylim': [0, 50]
                         }
     default_dicts[title] = dd
     show_max.append(False)
@@ -806,4 +821,64 @@ class Testing_ffhq_diffcam_exp_v1(unittest.TestCase):
     start_cmd_run(cmd_str)
     pass
 
+  def test_sampling_yaw_web(self, debug=True):
+    """
+    Usage:
+        source activate PyTorch-1.8
+
+        export CUDA_VISIBLE_DEVICES=0
+        export TIME_STR=0
+        export PYTHONPATH=.:./tl2_lib
+        python -c "from exp.tests.test_cips3d_inversion import Testing_ffhq_diffcam_exp_v1;\
+          Testing_ffhq_diffcam_exp_v1().test_sampling_yaw_web(debug=False)" \
+          --tl_opts port 8501
+
+    :return:
+    """
+    if 'CUDA_VISIBLE_DEVICES' not in os.environ:
+      os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    if 'TIME_STR' not in os.environ:
+      os.environ['TIME_STR'] = '0'
+    from tl2 import tl2_utils
+    from tl2.launch.launch_utils import \
+      (get_command_and_outdir, setup_outdir_and_yaml, get_append_cmd_str, start_cmd_run)
+
+    tl_opts_list = tl2_utils.parser_args_from_list(name="--tl_opts", argv_list=sys.argv, type='list')
+    tl_opts = ' '.join(tl_opts_list)
+    print(f'tl_opts:\n {tl_opts}')
+
+    command, outdir = get_command_and_outdir(self, func_name=sys._getframe().f_code.co_name, file=__file__)
+    argv_str = f"""
+                --tl_config_file exp/cips3d_inversion/configs/ffhq_diffcam_exp_v1.yaml
+                --tl_command {command}
+                --tl_outdir {outdir}
+                --tl_opts {tl_opts}
+                """
+    args, cfg = setup_outdir_and_yaml(argv_str, return_cfg=True)
+
+    n_gpus = len(os.environ['CUDA_VISIBLE_DEVICES'].split(','))
+
+    # script = "tl2_lib/tl2/proj/streamlit/scripts/run_web.py"
+    script = importlib.import_module('tl2.proj.streamlit.scripts.run_web').__file__
+    if debug:
+      cmd_str = f"""
+          python 
+            {script}
+            {get_append_cmd_str(args)}
+            --tl_debug
+            --tl_opts
+              """
+    else:
+      cmd_str_prefix = f"""
+              {os.path.dirname(sys.executable)}/streamlit run --server.port {cfg.port} 
+              {script}
+              --
+            """
+      cmd_str = f"""
+          {cmd_str_prefix}
+            {get_append_cmd_str(args)}
+            --tl_opts {tl_opts}
+        """
+    start_cmd_run(cmd_str)
+    pass
 
